aa0f830b64a24c057d7885d7d4965b959e76e453
Chau P <cqpham15@earlham.edu>
2017-02-05 22:02:56 -0500

    player object added with movement and shooting, enemy spawning added

---
+++ But It's A Meritocracy/Assets/Scripts/cursorScript.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+public var bullet : GameObject;
+
+function Update() {
+    // Get the rigidbody component
+    var r2d = GetComponent.<Rigidbody2D>();
+
+    // Move the spaceship when an arrow key is pressed
+    if (Input.GetKey("right"))
+        r2d.velocity.x = 10;
+    else if (Input.GetKey("left"))
+        r2d.velocity.x = -10;
+    else
+        r2d.velocity.x = 0;
+
+    // When the spacebar is pressed
+    if (Input.GetKeyDown("space")) {
+        // Create a new bullet at “transform.position” 
+        // Which is the current position of the ship
+        // Quaternion.identity = add the bullet with no rotation
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+} 
---
+++ But It's A Meritocracy/Assets/Scripts/enemyScript.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+ // Public variable that contains the speed of the enemy
+public var speed : int = -5;
+
+// Function called when the enemy is created
+function Start () {
+    // Get the rigidbody component
+    var r2d = GetComponent.<Rigidbody2D>();
+
+    // Add a vertical speed to the enemy
+    r2d.velocity.y = speed;
+
+    // Make the enemy rotate on itself
+    r2d.angularVelocity = Random.Range(-200, 200);
+}
+
+// Function called when the object goes out of the screen
+function OnBecameInvisible() {
+    // Destroy the enemy
+    Destroy(gameObject);
+}
