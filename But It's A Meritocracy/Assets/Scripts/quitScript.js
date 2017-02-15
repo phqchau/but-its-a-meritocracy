dac69c4604796a63f46e9e14e0449535925cf295
FLANNERY-LAPTOP\Flannery <flancor@swbell.net>
2017-02-14 21:12:37 -0500

    Scaling?

---
+++ But It's A Meritocracy/Assets/Scripts/quitScript.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+// Quits when escape key is pressed
+function Update() {
+    if (Input.GetKey ("escape")) {
+        quit();
+    }
+}
+
+// Quits when quit button is pressed
+function quit() {
+    Application.Quit();
+}
